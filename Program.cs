62f154dc564cf7c0dcd3544a293db2b3c8284b6b
Per Høgfeldt <hogfeldt38@gmail.com>
2019-10-08 11:12:14 +0200

    Initial code added

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RPNInterpreter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
