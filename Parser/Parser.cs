b664d62ac444c3442adb2a137d2dc0a520c83cfa
Per Høgfeldt <hogfeldt38@gmail.com>
2019-10-18 13:18:30 +0200

    The parser is now implemented

--- Parser/Parser.cs
+++ Parser/Parser.cs
@@ -8,8 +8,71 @@ namespace RPNParser {
         enum TokenName {Operator, Literal, Identifier};
 
         public IExpression parse(string input) {
-            Stack<Tuple<TokenName, string>> tokens = tokenization(Reverse(input));
-            return new Number(1);
+            Stack<Tuple<TokenName, string>> tokens = tokenization(input);
+            Tuple<TokenName, string> root = tokens.Peek();
+            IExpression result = null;
+            // Defining a variable is a bit special and should therefore be handled seperatly 
+            if(root.Item1 == TokenName.Identifier) {
+                root = tokens.Pop();
+                if (tokens.Count == 0) {
+                    result = new Variable(root.Item2);
+                } else {
+                    Tuple<TokenName, string> second = tokens.Pop();
+                    if(second.Item1 == TokenName.Operator && second.Item2 == "=") {
+                        result = new Define(buildSubtree(tokens), root.Item2);
+                    } else {
+                        throw new SyntaxErrorException("A variable cannot be followed by: "+ second.Item2);
+                    }
+                }
+            } else {
+                result = buildSubtree(tokens);
+            }
+            if (tokens.Count != 0) {
+                throw new SyntaxErrorException("Not valid syntax");
+            } 
+            return result;
+        }
+
+        private IExpression buildSubtree(Stack<Tuple<TokenName, string>> tokens) {
+            Tuple<TokenName, string> curr_t = tokens.Pop();
+            if(curr_t.Item1 == TokenName.Literal) {
+                return getNumSubTree(curr_t.Item2);
+            } else if (curr_t.Item1 == TokenName.Identifier) {
+                return new Variable(curr_t.Item2);
+            } else {
+                IExpression left = buildSubtree(tokens);
+                IExpression right = buildSubtree(tokens);
+                if(curr_t.Item2 == "+") {
+                    return new Plus(left, right);
+                } else if (curr_t.Item2 == "-") {
+                    return new Minus(right, left);
+                } else if (curr_t.Item2 == "*") {
+                    return new Multiply(left, right);
+                } else if (curr_t.Item2 == "/") {
+                    return new Divide(right, left);
+                } else {
+                throw new SyntaxErrorException("We don't know what just happend, good luck!");
+                }
+            } 
+        }
+
+        private IExpression getNumSubTree(string n) {
+            Stack<char> tokens = new Stack<char>();
+            char[] char_n = n.ToCharArray();
+            Array.Reverse(char_n);
+            foreach(char c in char_n) {
+                tokens.Push(c);
+            }
+            return generateNumSubTree(tokens);
+        }
+
+        private IExpression generateNumSubTree(Stack<char> tokens) {
+            char root = tokens.Pop();
+            if (tokens.Count == 0) {
+                return new Number(Convert.ToInt32(Char.GetNumericValue(root)));
+            }
+            Number child = (Number) generateNumSubTree(tokens);
+            return new Number(Convert.ToInt32(Char.GetNumericValue(root)), child);
         }
 
         private Stack<Tuple<TokenName, string>> tokenization(string input) {
@@ -29,17 +92,16 @@ namespace RPNParser {
             }
             return result;
         }
-
-        private string Reverse(string s)
-        {
-            char[] charArray = s.ToCharArray();
-            Array.Reverse(charArray);
-            return new string(charArray);
-        }
     }
 
     class InvalidTokenException : Exception {
         public InvalidTokenException(string message)
+        : base(message)
+        {
+        }
+    }
+    class SyntaxErrorException : Exception {
+        public SyntaxErrorException(string message)
         : base(message)
         {
         }
--- Program.cs
+++ Program.cs
@@ -1,18 +1,25 @@
 ﻿using System;
+using RPNInterpreter;
+using RPNParser;
 
-namespace RPNInterpreter
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Context c = new Context();
-            IExpression e = new Define(new Minus(new Number(5, new Number(0)), new Number(2)), "a");
-            e.Interpret(c);
-            IExpression e2 = new Plus(new Number(1), new Variable("a"));
-            e2.Interpret(c);
-            Console.WriteLine(c.s.Pop());
-
-        }
+        
+        Parser p = new Parser();
+        IExpression e1 = p.parse("120 = a");
+        Context c = new Context();
+        e1.Interpret(c);
+        IExpression e2 = p.parse("a 21 *");
+        e2.Interpret(c);
+        Console.WriteLine(c.s.Pop());
+        /*Context c = new Context();
+        IExpression e = new Define(new Minus(new Number(5, new Number(0)), new Number(2)), "a");
+        e.Interpret(c);
+        IExpression e2 = new Plus(new Number(1), new Variable("a"));
+        e2.Interpret(c);
+        Console.WriteLine(c.s.Pop());
+        */
     }
 }
