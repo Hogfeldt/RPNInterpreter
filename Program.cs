a152b46dff4790eabb642acb72b65052fced5144
Per Høgfeldt <hogfeldt38@gmail.com>
2019-10-18 13:36:04 +0200

    Implemented a simple REPL like input loop

--- Program.cs
+++ Program.cs
@@ -6,20 +6,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        
         Parser p = new Parser();
-        IExpression e1 = p.parse("120 = a");
         Context c = new Context();
-        e1.Interpret(c);
-        IExpression e2 = p.parse("a 21 *");
-        e2.Interpret(c);
-        Console.WriteLine(c.s.Pop());
-        /*Context c = new Context();
-        IExpression e = new Define(new Minus(new Number(5, new Number(0)), new Number(2)), "a");
-        e.Interpret(c);
-        IExpression e2 = new Plus(new Number(1), new Variable("a"));
-        e2.Interpret(c);
-        Console.WriteLine(c.s.Pop());
-        */
+        while(true) {
+            Console.Write('>');
+            string input = Console.ReadLine();
+            IExpression e = p.parse(input);
+            e.Interpret(c);
+            if (c.s.Count != 0) {
+                Console.WriteLine(c.s.Pop());
+            }
+        }
     }
 }
